4562fd3803a7c68a54f59fd49235ba2c22878f7d
Radhika B H <144422196+itsmeradhikaa@users.noreply.github.com>
2024-01-20 10:02:51 +0530

    Time: 655 ms (8.38%), Space: 16.7 MB (81.51%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(arr, ds, res, hashmap):
+            if len(ds) == len(arr):
+                if ds[:] not in res:
+                    res.append(ds[:])
+                return
+
+            for i in range(len(arr)):
+                if hashmap[i] == 1:
+                    continue
+                hashmap[i] = 1
+                ds.append(arr[i])
+                backtrack(arr, ds, res, hashmap)
+                ds.pop()
+                hashmap[i] = 0
+
+        res = []
+        hashmap = [0]*len(nums)
+        backtrack(nums, [], res, hashmap)
+        return res
